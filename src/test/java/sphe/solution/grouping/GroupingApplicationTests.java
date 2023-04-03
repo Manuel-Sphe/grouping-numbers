54247cd65acc51d3ca30a8e1d59e754e925b7d45
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-04-03 20:09:21 +0200

    collect-unit tests

--- src/test/java/sphe/solution/grouping/GroupingApplicationTests.java
+++ src/test/java/sphe/solution/grouping/GroupingApplicationTests.java
@@ -2,6 +2,7 @@ package sphe.solution.grouping;
 
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
@@ -18,41 +20,52 @@ class GroupingApplicationTests {
 	@Autowired // field injection
 	private  Number number;
 
+	private final Collection<Integer> expected = Arrays.asList(1,2,3);
+	private Collection<Integer> actual ;
+
+	@Before
+	public void setup(){
+		actual = new ArrayList<>();
+	}
 
 	@Test
-
-	public void testCollect(){
-		// Test Valid input
-
-		Collection<Integer> expected = new ArrayList<>();
-		expected.add(1);
-		expected.add(2);
-		expected.add(3);
-
-		//
-		Collection<Integer> actual = number.collect("1,2,3");
+	@DisplayName("Test Valid input")
+	void testCollect_1(){
+		actual = number.collect("1,2,3");
 		assertEquals(expected, actual);
-
-		// Test input with leading/trailing whitespace
+	}
+	@Test
+	@DisplayName("Input with spaces --valid")
+	void testCollect_2(){
 		actual = number.collect(" 1 , 2 , 3 ");
 		assertEquals(expected, actual);
+	}
 
-		// Test input with non-numeric characters
+	@Test
+	@DisplayName("Test with Non-numeric chars")
+	void testCollect_3(){
 		try {
 			number.collect("1,a,2");
 			fail("Expected IllegalArgumentException");
 		} catch (IllegalArgumentException e) {
 			assertEquals("a is not a valid integer", e.getMessage());
 		}
+	}
 
-		// Test empty input
+	@Test
+	@DisplayName("Test With empty String")
+	public void testCollect_4(){
 		try {
 			number.collect("");
 			fail("Expected IllegalArgumentException");
 		} catch (IllegalArgumentException e) {
 			assertEquals("Input must contain at least one number", e.getMessage());
 		}
+	}
 
+	@Test
+	@DisplayName("Test will null input")
+	public void testCollect_5(){
 		// Test null input
 		try {
 			number.collect(null);
@@ -60,7 +73,6 @@ class GroupingApplicationTests {
 		} catch (IllegalArgumentException e) {
 			assertEquals("Input cannot be null", e.getMessage());
 		}
-
 	}
 
 
