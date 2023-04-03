b01619bcd6c374815a5d7f8089eadcd5e6818a8e
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-04-03 19:13:20 +0200

    collect method done

--- src/main/java/sphe/solution/grouping/Number.java
+++ src/main/java/sphe/solution/grouping/Number.java
@@ -4,14 +4,38 @@ import org.springframework.stereotype.Component;
 import sphe.solution.grouping.numberrangesummarizer.NumberRangeSummarizer;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.regex.PatternSyntaxException;
 
 @Component
 public class Number implements NumberRangeSummarizer{
 
     @Override
-    public Collection<Integer> collect(String input){
-        return null;
+    public Collection<Integer> collect(String input) {
+        // Check for null input
+        if (input == null) {
+            throw new IllegalArgumentException("Input cannot be null");
+        }
+
+        Collection<Integer> collection = new ArrayList<>();
+        String[] number = input.split(",");
+
+        // Check for empty input
+        if (number.length == 1 && number[0].isEmpty()) {
+            throw new IllegalArgumentException("Input must contain at least one number");
+        }
+
+        for (final String item : number) {
+            try {
+                collection.add(Integer.parseInt(item));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(item + " is not a valid integer");
+            }
+        }
+
+        return collection;
     }
 
     @Override
